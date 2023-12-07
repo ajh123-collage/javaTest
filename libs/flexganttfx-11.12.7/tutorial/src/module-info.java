54334ebf3cf71f017a9bec0b48b71d2622c35af9
ajh123 <samrosoft36@gmail.com>
2023-12-07 11:43:03 +0000

    feat(ui): test flexganttfx

---
+++ libs/flexganttfx-11.12.7/tutorial/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2014 - 2021 DLSC Software & Consulting GmbH (dlsc.com)
+ *
+ * This file is part of FlexGanttFX.
+ */
+module com.flexganttfx.tutorials {
+    requires javafx.graphics;
+    requires com.flexganttfx.view;
+    exports com.flexganttfx.tutorial;
+}
--- src/main/java/uk/minersonline/test/Main.java
+++ src/main/java/uk/minersonline/test/Main.java
@@ -1,5 +1,7 @@
 package uk.minersonline.test;
 
+import com.flexganttfx.extras.GanttChartStatusBar;
+import com.flexganttfx.extras.GanttChartToolBar;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,10 +10,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.flexganttfx.model.Activity;
+import com.flexganttfx.model.Layer;
+import com.flexganttfx.model.Row;
+import com.flexganttfx.model.activity.MutableActivityBase;
+import com.flexganttfx.view.GanttChart;
+
 
 public class Main extends Application {
     private int count = 0;
@@ -20,11 +32,52 @@ public class Main extends Application {
         launch();
     }
 
+    class ModelObject<
+            P extends Row<?,?,?>, // Type of parent row
+            C extends Row<?,?,?>, // Type of child rows
+            A extends Activity> extends Row<P, C, A> { }
+
+    class Fleet extends ModelObject<Row<?,?,?>, Aircraft, Activity> { }
+
+    class Aircraft extends ModelObject<Fleet, Fleet, Flight> { }
+
+    class Flight extends MutableActivityBase<Object> { }
+
     @Override
     public void start(Stage stage) throws IOException {
-        URL file = Main.class.getClassLoader().getResource("main.fxml");
-        Parent root = FXMLLoader.load(file);
-        Scene scene = new Scene(root, 640, 480);
+        Fleet fleet = new Fleet();
+        fleet.setExpanded(true);
+        GanttChart<ModelObject<?,?,?>> gantt = new GanttChart<>(fleet);
+
+        // Layers based on service type.
+        Layer cargoLayer = new Layer("Cargo");
+        Layer trainingLayer = new Layer("Training");
+        Layer charterLayer = new Layer("Charter");
+        gantt.getLayers().addAll(cargoLayer, trainingLayer, charterLayer);
+
+        // Create the aircrafts.
+        Aircraft aircraft1 = new Aircraft();
+        Aircraft aircraft2 = new Aircraft();
+
+        // Add the aircrafts to the fleet.
+        fleet.getChildren().addAll(aircraft1, aircraft2);
+
+        // Create the flights
+        Flight flight1 = new Flight(); // a cargo flight
+        Flight flight2 = new Flight(); // a training flight
+        Flight flight3 = new Flight(); // a charter flight
+
+        aircraft1.addActivity(cargoLayer, flight1);
+        aircraft1.addActivity(trainingLayer, flight2);
+        aircraft2.addActivity(charterLayer, flight3);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(new GanttChartToolBar<>(gantt));
+        borderPane.setCenter(gantt);
+        borderPane.setBottom(new GanttChartStatusBar<>(gantt));
+
+        stage.setTitle("Test");
+        Scene scene = new Scene(borderPane, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
