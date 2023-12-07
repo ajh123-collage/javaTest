ff6669bda3a15a53e690f7780a2b6d8d24438da2
ajh123 <samrosoft36@gmail.com>
2023-12-07 09:59:31 +0000

    feat(ui): use fxml with working button

--- src/main/java/uk/minersonline/test/Main.java
+++ src/main/java/uk/minersonline/test/Main.java
@@ -1,20 +1,39 @@
 package uk.minersonline.test;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class Main extends Application {
+    private int count = 0;
+
     public static void main(String[] args) {
         launch();
     }
 
     @Override
-    public void start(Stage stage) {
-        Label l = new Label("Hello, JavaFX");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws IOException {
+        URL file = Main.class.getClassLoader().getResource("main.fxml");
+        Parent root = FXMLLoader.load(file);
+        Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    private void printCount(ActionEvent event) {
+        count = count + 1;
+        event.consume();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Button clicked %s times!".formatted(count), ButtonType.OK);
+        alert.show();
     }
 }
